10998ab571c48cb3c46d1dbcb37e09e364e8e223
KadriyeAl <kadriyealsancak@gmail.com>
2024-10-04 19:59:16 +0300

    Utility,libraries

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -1,7 +1,7 @@
 package Utility;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -16,7 +16,7 @@ import java.time.Duration;
 
 public class BaseDriver {
 
-    public static Logger logEkle= LogManager.getLogger();
+    //public static Logger logEkle= LogManager.getLogger();
     //logları ekleyeceğim kuyrugu başlattım
     public static WebDriver driver;
     public static WebDriverWait wait;
@@ -26,10 +26,10 @@ public class BaseDriver {
        // System.out.println("Başlangıç işlemleri yapılıyor");
 
         driver=new ChromeDriver();
-        logEkle.info("Driver Başlatıldı");
+       // logEkle.info("Driver Başlatıldı");
 
         //hata oluşmuş olsaydı
-        logEkle.error("Driver oluşturulurken hata oluştu");
+        //logEkle.error("Driver oluşturulurken hata oluştu");
 
         driver.manage().window().maximize(); // Ekranı max yapıyor.
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20)); // 20 sn mühlet: sayfayı yükleme mühlet
@@ -53,7 +53,7 @@ public class BaseDriver {
 
         WebElement email=driver.findElement(By.id("input-email"));
         email.sendKeys("kadriyealsancak@gmail.com");
-        logEkle.info("şu anda"+"kadriyealsancak@gmail.com"+"isimli user login olmak için gönderildi");
+       // logEkle.info("şu anda"+"kadriyealsancak@gmail.com"+"isimli user login olmak için gönderildi");
 
 
         WebElement password=driver.findElement(By.id("input-password"));
@@ -63,27 +63,27 @@ public class BaseDriver {
         loginBtn.click();
 
         wait.until(ExpectedConditions.titleIs("My Account"));
-        logEkle.debug("Login işlemine geçiliyor");
+        //logEkle.debug("Login işlemine geçiliyor");
 
         Assert.assertTrue(driver.getTitle().equals("My Account"), "Login olunamadı");
-        logEkle.info("login işlemi başarıyla yapıldı");
-        logEkle.warn("login işlemlerinde kadriyealsancak@gmail.com kullanıcıda önemli hata oluştu");
+       // logEkle.info("login işlemi başarıyla yapıldı");
+       // logEkle.warn("login işlemlerinde kadriyealsancak@gmail.com kullanıcıda önemli hata oluştu");
     }
     @BeforeMethod
     public void BeforeMethod()
     {
-        logEkle.info("Metod çalışmaya başladı");
+       // logEkle.info("Metod çalışmaya başladı");
 
 
     }
     @AfterMethod
     public void AfterMethod(ITestResult sonuc)
     {
-       logEkle.info(sonuc.getName()+"Metıd çalışması tamamlandı");
-       logEkle.info(sonuc.getStatus()==1 ?"Passed" : "failed");
+      // logEkle.info(sonuc.getName()+"Metıd çalışması tamamlandı");
+      // logEkle.info(sonuc.getStatus()==1 ?"Passed" : "failed");
 
        //çok önemli hata oldu/fatal eror
-        logEkle.fatal(sonuc.getName()+"Metod da çok önemli bir hata oldu");
+        //logEkle.fatal(sonuc.getName()+"Metod da çok önemli bir hata oldu");
     }
 
 }
