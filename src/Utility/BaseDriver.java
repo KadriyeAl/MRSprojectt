8c0cbe5bd17d9e1be47b36aeb4eb26a6beba1452
KadriyeAl <kadriyealsancak@gmail.com>
2024-10-04 20:23:24 +0300

    Utility,basedriver oturumAcmaiptal

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -36,7 +36,7 @@ public class BaseDriver {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20)); // 20 sn mühlet: elementi bulma mühleti
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
-        Login();
+
     }
 
     @AfterClass
@@ -47,43 +47,7 @@ public class BaseDriver {
         driver.quit();
     }
 
-    public void Login() {
-        driver.get("https://opencart.abstracta.us/index.php?route=account/login");
-        Tools.Bekle(2);
-
-        WebElement email=driver.findElement(By.id("input-email"));
-        email.sendKeys("kadriyealsancak@gmail.com");
-       // logEkle.info("şu anda"+"kadriyealsancak@gmail.com"+"isimli user login olmak için gönderildi");
-
-
-        WebElement password=driver.findElement(By.id("input-password"));
-        password.sendKeys("4521354Ka");
-
-        WebElement loginBtn=driver.findElement(By.xpath("//input[@value='Login']"));
-        loginBtn.click();
-
-        wait.until(ExpectedConditions.titleIs("My Account"));
-        //logEkle.debug("Login işlemine geçiliyor");
-
-        Assert.assertTrue(driver.getTitle().equals("My Account"), "Login olunamadı");
-       // logEkle.info("login işlemi başarıyla yapıldı");
-       // logEkle.warn("login işlemlerinde kadriyealsancak@gmail.com kullanıcıda önemli hata oluştu");
-    }
-    @BeforeMethod
-    public void BeforeMethod()
-    {
-       // logEkle.info("Metod çalışmaya başladı");
-
 
     }
-    @AfterMethod
-    public void AfterMethod(ITestResult sonuc)
-    {
-      // logEkle.info(sonuc.getName()+"Metıd çalışması tamamlandı");
-      // logEkle.info(sonuc.getStatus()==1 ?"Passed" : "failed");
 
-       //çok önemli hata oldu/fatal eror
-        //logEkle.fatal(sonuc.getName()+"Metod da çok önemli bir hata oldu");
-    }
 
-}
